9397f10d7f7ac0a112363b17be476edd5acb6478
stella-gao <stella.gao.89@gmail.com>
2016-06-08 17:10:26 -0500

    hello world

---
+++ src/Stella.java
@@ -0,0 +1,9 @@
+/**
+ * Created by xigao on 6/8/2016.
+ */
+public class Stella {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("Have fun! hahaha~~");
+    }
+}
