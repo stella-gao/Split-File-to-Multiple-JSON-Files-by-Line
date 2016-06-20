ca7ed72d435c1179000915f6de055fc38dae8560
Stella Gao <stella.gao.89@gmail.com>
2016-06-20 10:51:16 -0500

    Delete Stella.java

--- src/Stella.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xigao on 6/8/2016.
- */
-public class Stella {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
-        System.out.println("Have fun! hahaha~~");
-    }
-}
